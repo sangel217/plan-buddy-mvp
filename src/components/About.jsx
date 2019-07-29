7fc9fdee0c829f43df6d426c9e6a29dd4f5d1b57
Student <example@example.com>
2019-07-29 16:45:31 -0700

    add styling and content to About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,96 @@ import React from "react";
 function About(){
     return(
         <div>
-            <h3>About Us</h3>
+            <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                <strong><h2 className="text-center">We are here to help you!</h2></strong>
+
+                <ul className="timeline">
+                    <li>
+                    <div className="timeline-image">
+                        <img className="img-circle img-responsive" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz3htS9jowwrJc7qeZr-i6XirzONBOBBLQfx6X29jS-FCxH6gi" alt=""/>
+                    </div>
+                    <div className="timeline-panel">
+                        <div className="timeline-heading">
+
+                        <h4 className="subheading">Review all women's clinics in Seattle!</h4>
+                        </div>
+                        <div className="timeline-body">
+                        <p className="text-muted">
+                        There are more than 10 women's clinics in Seattle area. You can find all information on our website.
+                        </p>
+                        </div>
+                    </div>
+                    <div className="line"></div>
+                    </li>
+                    <li className="timeline-inverted">
+                    <div className="timeline-image">
+                        <img className="img-circle img-responsive" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKE-euI6pHx1sVo0EH-ziy5EmFg5W9Bx9Wrk09IAFblnuo0vq3Vg" alt=""/>
+                    </div>
+                    <div className="timeline-panel">
+                        <div className="timeline-heading">
+
+                        <h4 className="subheading">Read articles!</h4>
+                        </div>
+                        <div className="timeline-body">
+                        <p className="text-muted">
+                            We have a bunch of usefull articles to keep you inform about...</p>
+                        </div>
+                    </div>
+                    <div className="line"></div>
+                    </li>
+                    <li>
+                    <div className="timeline-image">
+                        <img className="img-circle img-responsive" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv3HHGUQ1XW_UI1sIjPE-V9UXSXZ1h_Fp4pY1On76FYO3p7RRL" alt=""/>
+                    </div>
+                    <div className="timeline-panel">
+                        <div className="timeline-heading">
+
+                        <h4 className="subheading">Be active on forum!</h4>
+                        </div>
+                        <div className="timeline-body">
+                        <p className="text-muted">
+                        On our forum you can discuss your experiences with different clinics, tips on health and women's options.
+                        </p>
+                        </div>
+                    </div>
+                    <div className="line"></div>
+                    </li>
+                    <li className="timeline-inverted">
+                    <div className="timeline-image">
+                        <img className="img-circle img-responsive" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtOvqyPiT1txUObqk97js8bxa4YIoxj0rm6ql1nK94ojKvkGIDEQ" alt=""/>
+                    </div>
+                    <div className="timeline-panel">
+                        <div className="timeline-heading">
+
+                        <h4 className="subheading">Save your favorite clinics.</h4>
+                        </div>
+                        <div className="timeline-body">
+                        <p className="text-muted">
+                            As you look trough the clinics you can save the ones of your preference.</p>
+                        </div>
+                    </div>
+                    <div className="line"></div>
+                    </li>
+                    <li>
+                    <div className="timeline-image">
+                        <img className="img-circle img-responsive" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy4yI2CyLZ7wI1YDUbqawPNGhnAVBQA7YSgGrxvQP7CPgsJtUw" alt=""/>
+                    </div>
+                    <div className="timeline-panel">
+                        <div className="timeline-heading">
+
+                        <h2 className="subheading">Stay Healthy!</h2>
+                        <h2 className="subheading">Be Kind!</h2>
+                        </div>
+                        <div className="timeline-body">
+                        </div>
+                    </div>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
