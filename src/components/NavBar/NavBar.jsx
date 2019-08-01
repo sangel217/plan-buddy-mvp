f521fed919f8f1e18e385072ae0233a1c78b26c5
Student <example@example.com>
2019-08-01 15:08:24 -0700

    fix some styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,29 +6,24 @@ import './NavBar.css';
 function NavBar(){
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark">
+            <nav className="navbar navbar-expand-sm navbar-dark">
                 <Link to="/" className="navbar-brand" >
                     <h3 className="my-heading ">Plan<span className="bg-main">Buddy</span></h3>
                 </Link>
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="fa fa-bars mfa-white"></span>
-                </button>
 
                 <div className="collapse navbar-collapse" id="navbarResponsive">
-
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-link">
-                    <Link to="/clinics" className="btn btn-primary btn-block btn-register" >Clinics</Link>
-                    </li>
-                    <li className="nav-link">
-                    <Link to="/references" className="btn btn-primary btn-block btn-register">References</Link>
-                    </li>
-                    <li className="nav-link">
-                    <Link to="/about" id="btn1" className="btn btn-primary btn-block btn-register">About</Link>
-                    </li>
-                </ul>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-link">
+                        <Link to="/clinics" className="btn btn-primary btn-block btn-register" >Clinics</Link>
+                        </li>
+                        <li className="nav-link">
+                        <Link to="/references" className="btn btn-primary btn-block btn-register">References</Link>
+                        </li>
+                        <li className="nav-link">
+                        <Link to="/about" id="btn1" className="btn btn-primary btn-block btn-register">About</Link>
+                        </li>
+                    </ul>
                 </div>
-
             </nav>
         </div>
     );
